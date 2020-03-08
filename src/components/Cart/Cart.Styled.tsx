ca5951c82178a1d361022478845d87091e7e7445
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-03-08 12:00:17 -0700

    overflow y

--- src/components/Cart/Cart.Styled.tsx
+++ src/components/Cart/Cart.Styled.tsx
@@ -4,10 +4,11 @@ const Cart = styled.div`
   position: fixed;
   right: 0;
   bottom: 0;
-  top: 1.5em;
+  top: 1.3em;
   width: 100%;
   max-width: 20em;
   background: tan;
+  overflow-y: scroll;
 `
 
 export default {
